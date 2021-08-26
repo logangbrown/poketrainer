dd05e50f75b38dcc5f5c0a674a5ba029d418ffe8
Logan Brown <loganb@wc-3.com>
2021-08-25 18:34:34 -0600

    Testing basic connection to PokeAPI

--- js/index.controller.js
+++ js/index.controller.js
@@ -1,3 +1,8 @@
 angular.module('poketrainer').controller('IndexCtrl', function ($scope, $http, $routeParams, $location) {
+    $scope.pokemon = {};
 
+    $http.get("https://pokeapi.co/api/v2/pokemon/charmander")
+        .then(function (response) {
+            $scope.pokemon.charmander = response;
+        });
 });
