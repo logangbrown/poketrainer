c331f2163a61d8ac287441a622fdeb90d68bff68
Logan Brown <loganb@wc-3.com>
2021-08-25 19:03:43 -0600

    fixing buttons

--- js/index.controller.js
+++ js/index.controller.js
@@ -1,24 +1,24 @@
 angular.module('poketrainer').controller('IndexCtrl', function ($scope, $http, $routeParams, $location) {
     $scope.pokemon = {};
     $scope.pokemon.types = {
-        'normal': {'name': 'Normal'},
-        'fire': {'name': 'Fire'},
-        'water': {'name': 'Water'},
-        'grass': {'name': 'Grass'},
-        'electric': {'name': 'Electric'},
-        'ice': {'name': 'Ice'},
-        'fighting': {'name': 'Fighting'},
-        'poison': {'name': 'Poison'},
-        'ground': {'name': 'Ground'},
-        'flying': {'name': 'Flying'},
-        'psychic': {'name': 'Psychic'},
-        'bug': {'name': 'Bug'},
-        'rock': {'name': 'Rock'},
-        'ghost': {'name': 'Ghost'},
-        'dark': {'name': 'Dark'},
-        'dragon': {'name': 'Dragon'},
-        'steel': {'name': 'Steel'},
-        'fairy': {'name': 'Fairy'}
+        'normal': {'index': 'normal', 'name': 'Normal'},
+        'fire': {'index': 'fire', 'name': 'Fire'},
+        'water': {'index': 'water', 'name': 'Water'},
+        'grass': {'index': 'grass', 'name': 'Grass'},
+        'electric': {'index': 'electric', 'name': 'Electric'},
+        'ice': {'index': 'ice', 'name': 'Ice'},
+        'fighting': {'index': 'fighting', 'name': 'Fighting'},
+        'poison': {'index': 'poison', 'name': 'Poison'},
+        'ground': {'index': 'ground', 'name': 'Ground'},
+        'flying': {'index': 'flying', 'name': 'Flying'},
+        'psychic': {'index': 'psychic', 'name': 'Psychic'},
+        'bug': {'index': 'bug', 'name': 'Bug'},
+        'rock': {'index': 'rock', 'name': 'Rock'},
+        'ghost': {'index': 'ghost', 'name': 'Ghost'},
+        'dark': {'index': 'dark', 'name': 'Dark'},
+        'dragon': {'index': 'dragon', 'name': 'Dragon'},
+        'steel': {'index': 'steel', 'name': 'Steel'},
+        'fairy': {'index': 'fairy', 'name': 'Fairy'}
     }
 
     $http.get("https://pokeapi.co/api/v2/pokemon/charmander")
